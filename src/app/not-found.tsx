54e544528788c0cead339db43e41d201a49aca2e
Firdous Alam <firdous.alam2058@gmail.com>
2025-08-01 11:37:50 +0530

    basic code

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+// app/not-found.js
+export default function NotFound() {
+    return (
+        <div style={{ textAlign: 'center', marginTop: '50px' }}>
+            <h1>404 - Page Not Found</h1>
+            <p>Sorry, we couldn’t find the page you’re looking for.</p>
+        </div>
+    );
+}
--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -6,8 +6,14 @@ export default function Sidebar() {
       <h2 className="text-xl font-bold mb-6">My Dashboard</h2>
       <nav className="flex flex-col gap-2">
         <Link href="/dashboard" className="hover:bg-blue-600 p-2 rounded">Dashboard</Link>
-        <Link href="/dashboard/profile" className="hover:bg-blue-600 p-2 rounded">Profile</Link>
-        <Link href="/dashboard/settings" className="hover:bg-blue-600 p-2 rounded">Settings</Link>
+        <Link href="/physiotherapist" className="hover:bg-blue-600 p-2 rounded">Physiotherapist</Link>
+        <Link href="/user" className="hover:bg-blue-600 p-2 rounded">User</Link>
+        <Link href="/booking" className="hover:bg-blue-600 p-2 rounded">Booking</Link>
+        <Link href="/otherSpecialist" className="hover:bg-blue-600 p-2 rounded">Other Specialist</Link>
+        <Link href="/feature" className="hover:bg-blue-600 p-2 rounded">Feature</Link>
+        <Link href="/product" className="hover:bg-blue-600 p-2 rounded">Product</Link>
+        <Link href="/service" className="hover:bg-blue-600 p-2 rounded">Service</Link>
+        <Link href="/testimonial" className="hover:bg-blue-600 p-2 rounded">Testimonial</Link>
       </nav>
     </aside>
   );
